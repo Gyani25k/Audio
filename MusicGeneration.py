2343eb1980fd28c92887c4012b99f20e485b4233
Gyanender Kumar <72467942+Gyani25k@users.noreply.github.com>
2024-03-04 13:47:45 +0530

    Add files via upload

---
+++ MusicGeneration.py
@@ -0,0 +1,51 @@
+import os
+from dotenv import load_dotenv
+import replicate
+import streamlit as st
+
+load_dotenv()
+
+API_TOKEN = os.getenv('REPLICATE_API_TOKEN')
+
+api = replicate.Client(api_token=API_TOKEN)
+
+st.title("Music Generation Tool")
+
+# User inputs
+prompt = st.text_input("Enter your prompt:", "Engergetic EDM")
+
+model_options = ["small", "melody", "large"]
+
+model = st.selectbox("Select Model:", model_options, index=0)
+
+strategy_options = ["loudness", "clip", "peak", "rms"]
+
+strategy = st.selectbox("Select Normalization Strategy:", strategy_options, index=0)
+
+duration = st.slider("Select Duration (in seconds):", min_value=1, max_value=300, value=8)
+
+# Create a button to generate music
+if st.button("Generate Music"):
+    processing_message = st.empty()
+    processing_message.info("Generating...")
+
+    # Run Replicate API
+    output = api.run(
+        "aussielabs/musicgen:11bbae94fb523ecaab2b87ec074fd5b668f20d2b84030c093ed5d5ba8f6f4df1",
+        input={
+
+            "prompt": prompt,
+            "duration": duration,
+            "model_version": model,
+            "normalization_strategy": strategy,
+        }
+    )
+
+    processing_message.empty()
+    st.success("Music generated successfully!")
+
+    # Display the generated audio
+    st.subheader("Generated Audio")
+    generated_audio = output
+    if generated_audio:
+        st.audio(generated_audio, format='audio/wav')
