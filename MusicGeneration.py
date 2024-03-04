49a548027279463429c3fb157f1970164113e6fb
Gyanender Kumar <72467942+Gyani25k@users.noreply.github.com>
2024-03-04 13:49:16 +0530

    Update MusicGeneration.py

--- MusicGeneration.py
+++ MusicGeneration.py
@@ -1,11 +1,9 @@
 import os
-from dotenv import load_dotenv
 import replicate
 import streamlit as st
 
-load_dotenv()
 
-API_TOKEN = os.getenv('REPLICATE_API_TOKEN')
+API_TOKEN = 'r8_U87hJHYC7Xv6JZxryOqfFzPt7PBHyXd08Faj2'
 
 api = replicate.Client(api_token=API_TOKEN)
 
